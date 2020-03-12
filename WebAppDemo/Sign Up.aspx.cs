3a6a06fe61b9c71370f71666fb47f2dab64cbd9b
William Wakeford <William.Wakeford@worthing.ac.uk>
2020-03-12 09:27:38 +0000

    Sign Up

--- WebAppDemo/Sign Up.aspx.cs
+++ WebAppDemo/Sign Up.aspx.cs
@@ -16,40 +16,8 @@ namespace WebAppDemo
 
         }
 
-        protected void FnameTb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void LnameTb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void UsernameTb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void EmailTb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void PasswordTb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string Fname = FnameTb.Text;
-            string Lname = LnameTb.Text;
-            string Username = UsernameTb.Text;
-            string Email = EmailTb.Text;
-            string Password = PasswordTb.Text;
 
             OleDbConnection con = new OleDbConnection();
 
@@ -57,16 +25,17 @@ namespace WebAppDemo
             con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\wcfs5\home\William.Wakeford\Computer Science\NEA PROJECT\Database\Database3.accdb";
             con.Open();
             cmd.Connection = con;
-            cmd.CommandText = @"INSERT INTO User (UserName, FirstName, LastName, Email, Password) VALUES (@uname, @fname, @lname, @email, @password)";
+            cmd.CommandText = @"INSERT INTO [User] (UserName, FirstName, LastName, Email, Password) VALUES (@uname, @fname, @lname, @email, @password)";
 
 
             if(con.State == ConnectionState.Open)
             {
-                cmd.Parameters.Add("@uname", OleDbType.VarChar).Value = Username;
-                cmd.Parameters.Add("@fname", OleDbType.VarChar).Value = Fname;
-                cmd.Parameters.Add("@lname", OleDbType.VarChar).Value = Lname;
-                cmd.Parameters.Add("@email", OleDbType.VarChar).Value = Email;
-                cmd.Parameters.Add("@password", OleDbType.VarChar).Value = Password;
+                cmd.Parameters.AddWithValue("@uname", UsernameTb.Text);
+                cmd.Parameters.AddWithValue("@fname", FnameTb.Text);
+                cmd.Parameters.AddWithValue("@lname", LnameTb.Text);
+                cmd.Parameters.AddWithValue("@email", EmailTb.Text);
+                cmd.Parameters.AddWithValue("@password", PasswordTb.Text);
+
                 try
                 {
                     cmd.ExecuteNonQuery();
